b79ea6eb3cc8459ad5caf12f86361db048fdfde2
hamjah <cee.pei14@gmail.com>
2024-10-08 18:50:47 +0700

    Frontend: - Interaksi dengan Back-End, Pemanggilan API Post

--- frontend/src/container/BlogPost/BlogPost.jsx
+++ frontend/src/container/BlogPost/BlogPost.jsx
@@ -6,7 +6,57 @@ import axios from 'axios';
 
 class BlogPost extends Component {
     state = {
-        post:[]
+        post:[],
+        formBlogPost:{
+            id:1,
+            title:'',
+            body:'',
+            userId:1,
+        } 
+    }
+ 
+    getPostApi = () => { 
+        axios.get('http://localhost:3000/posts?_sort=id&_order=desc')
+        .then((res) => {  
+            this.setState({
+                post:res.data
+            })
+        })
+    }
+
+    handleSaveDataToApi = (data) => {
+        axios.post(`http://localhost:3000/posts/`, data)
+        .then((res) => { 
+            this.getPostApi()
+        }, (err) => {
+            console.log(err)
+        }) 
+    }
+
+    handleRevomeDataToApi = (iddata) => { 
+        axios.delete(`http://localhost:3000/posts/${iddata}`)
+        .then((res) => {  
+            console.log(res)
+            this.getPostApi()
+        })
+    }
+ 
+    handleFormChange = (event) =>{
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let iddata = new Date().getTime();
+        
+        formBlogPostNew['id'] = iddata;
+        formBlogPostNew[event.target.name] = event.target.value;
+
+        this.setState({
+            formBlogPost: formBlogPostNew
+        }, ()=> {
+            // console.log(this.state.formBlogPost)
+        })
+    }
+
+    handleSubmit = (event) => {  
+        this.handleSaveDataToApi(this.state.formBlogPost)
     }
 
     componentDidMount(){
@@ -21,29 +71,33 @@ class BlogPost extends Component {
         this.getPostApi()
     }
 
-    getPostApi = () => { 
-        axios.get('http://localhost:3000/posts')
-        . then((res) => { 
-            this.setState({
-                post:res.data
-            })
-        })
-    }
-
-    handleRevome = (iddata) => { 
-        axios.delete(`http://localhost:3000/posts//${iddata}`)
-        . then((res) => {  
-            console.log(res);
-            this.getPostApi()
-        })
-    }
-
     render(){
         return(
             <Fragment>
+                <div className="form-add-post"> 
+                    {/* <form onSubmit={this.handleSubmit}> */} 
+                        <div>
+                            <label htmlFor="title">Title</label> 
+                            <div>
+                                <input type="text" name="title" placeholder="Add Title" onChange={this.handleFormChange} />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="body">Blog Content</label> 
+                            <div>
+                                <textarea name="body" id="" cols="15" row="10" placeholder="Add Blog Content" onChange={this.handleFormChange}></textarea>
+                            </div>
+                        </div>
+                        <div>
+                            <button  className="btn-submit" onClick={this.handleSubmit} >Simpan</button>
+                        </div>
+                    {/* </form> */}
+                </div>
+                <br />
+
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} data={post} remove={this.handleRevome} />
+                        return <Post key={post.id} data={post} remove={this.handleRevomeDataToApi} />
                     })
                 }
                 
