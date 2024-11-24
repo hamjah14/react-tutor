420bb1c0ffc0af9fe015d4e66922544830631e7d
hamjah <cee.pei14@gmail.com>
2024-11-25 01:54:49 +0700

    backend: Mengupload File di express

--- backend/express-mysql/index.js
+++ backend/express-mysql/index.js
@@ -1,14 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
-// routes
+const cors = require("cors"); 
+  
+const upload = require('./src/config/multer');
 const userRoute = require("./src/route/userRoute")
 
+app.use(cors()); 
 app.use(express.json());
-app.use("/images", express.static('public/images/post'));
 app.use("/v1/user", userRoute);
- 
+  
+app.use("/images/post", express.static("public/images/post/"));
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(201).json({
+        "message": "Upload berhasil"
+    })
+});
+
+// error handling yang tidak di-handle fungsi
+app.use((error, req, res, next) => {
+    const status = error.status || 500
+    const message = error.message
+    const data = error.data
+
+    res.status(status).json({
+        "message": message,
+        "data": data
+    })
+});
+
 app.listen(process.env.PORT, () => {
     console.log("Server berhasil dijalankan pada port ", process.env.PORT);
 })
---
+++ backend/express-mysql/src/config/multer.js
@@ -0,0 +1,46 @@
+const multer = require("multer");
+const fs = require('fs'); 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getUTCMonth() + 1;
+        let folderName = "./assets/images/" + year + "/" + month;
+  
+        // if (!fs.existsSync(folderName)){
+        //     fs.mkdirSync(folderName, { recursive: true });
+        // }
+         
+        folderName = "./public/images/post";
+        
+        cb(null, folderName );
+    },
+    filename: (req, file, cb) => {
+        const time = new Date().getTime();
+        const fileName = file.originalname; 
+
+        cb(null, time + "-" + fileName);
+    }
+})
+ 
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === "image/jpg" || 
+        file.mimetype === "image/jpeg" || 
+        file.mimetype === "image/png"){
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+ 
+const maxSize = 2 * 1024 * 1024;
+// const maxSize = 2 * 1024;
+
+const upload = multer({
+    storage: storage, 
+    limits: { fileSize: maxSize }, 
+    fileFilter: fileFilter
+});
+ 
+module.exports = upload;
